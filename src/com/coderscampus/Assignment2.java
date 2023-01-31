6b2b3d843c4efcfad267323d282a50042c5bb165
PaulG2392 <pgonzalez2392@gmail.com>
2023-01-30 21:34:15 -0500

    v0.002

--- src/com/coderscampus/Assignment2.java
+++ src/com/coderscampus/Assignment2.java
@@ -1,11 +1,32 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		// random num generator
+		
 		// collect user input
+		
+		// variables
+		
+			int guess = -1;
+			int attempt = 0;
+		
+		// inputs
+			Scanner keyboard = new Scanner(System.in);
+			Random random = new Random();
+			int secretnumber = random.nextInt(100) + 1;
+			
+			System.out.println("Guess a number between 1 and 100");
+			System.out.println(secretnumber);
+			
+			while(attempt < 5) {
+				
+			}
 
 	}
 
