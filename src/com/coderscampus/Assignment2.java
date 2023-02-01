a013a6339c13fd8c731b42aab21e3caa5297331d
PaulG2392 <pgonzalez2392@gmail.com>
2023-01-31 21:34:33 -0500

    latest assign ver. for submitting.

--- src/com/coderscampus/Assignment2.java
+++ src/com/coderscampus/Assignment2.java
@@ -18,28 +18,33 @@ public class Assignment2 {
 		// inputs
 			Scanner keyboard = new Scanner(System.in);
 			Random random = new Random();
-			int secretnumber = random.nextInt(100) + 1;
+			int secretNumber = random.nextInt(100) + 1;
 			
 			System.out.println("Guess a number between 1 and 100");
-			System.out.println(secretnumber);
+			//System.out.println(secretNumber);
 			
 			while(attempt < 5) {
 				guess = keyboard.nextInt();
-				attempt ++;
 				
-				if (guess == secretnumber) { System.out.println("You Win!");
-				break; }
+				if (guess <= 100 && guess >= 1) {
 				
-				else if (guess < secretnumber) {System.out.println("Please pick a higher number");
+					if (guess == secretNumber) { System.out.println("You Win!");
+					break; }
+				
+					else if (guess < secretNumber) {System.out.println("Please pick a higher number");
+					}
+				
+					else if (guess > secretNumber) {System.out.println("Please pick a lower number");
+					}
+				attempt++;}
+				else {System.out.println("Not a valid choice");
 				}
 				
-				else if (guess > secretnumber) {System.out.println("Please pick a lower number");
+				if (attempt == 5) {System.out.println("You lose. The number to guess was " + secretNumber );
 				}
-				
-				if (attempt == 5) {System.out.println("You lose. Please try again");
-				break; }
-				
 
 	        }
+			keyboard.close();
 
+}
 }
